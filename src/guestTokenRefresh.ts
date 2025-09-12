0f43741a64f79e006c5be7bfac000d88a63a2c9c
Vitor Avila <vitor.avila@preset.io>
2025-09-12 16:10:02 -0300

    fix: Use jwtDecode to parse the token

--- src/guestTokenRefresh.ts
+++ src/guestTokenRefresh.ts
@@ -1,11 +1,12 @@
+import { jwtDecode } from "jwt-decode";
+
 export const REFRESH_TIMING_BUFFER_MS = 5000 // refresh guest token early to avoid failed superset requests
 export const MIN_REFRESH_WAIT_MS = 10000 // avoid blasting requests as fast as the cpu can handle
 export const DEFAULT_TOKEN_EXP_MS = 300000 // (5 min) used only when parsing guest token exp fails
 
 // when do we refresh the guest token?
 export function getGuestTokenRefreshTiming(currentGuestToken: string) {
-  // atob converts a string from base64 https://developer.mozilla.org/en-US/docs/Web/API/atob
-  const parsedJwt = JSON.parse(atob(currentGuestToken.split('.')[1]).toString());
+  const parsedJwt = jwtDecode<Record<string, any>>(currentGuestToken);
   // if exp is int, it is in seconds, but Date() takes milliseconds
   const exp = new Date(/[^0-9\.]/g.test(parsedJwt.exp) ? parsedJwt.exp : parseFloat(parsedJwt.exp) * 1000);
   const isValidDate = exp.toString() !== 'Invalid Date';
